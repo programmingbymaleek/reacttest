aa45c6c113790baf783b4793b278abf8eed02524
Wisdom Maliki <wisdommaliki19@gmail.com>
2023-11-04 16:37:19 -0600

    added dave to branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <p>This is just a test branch 123....</p>
         <p>This is a fantastic project</p>
-        <p> I just added mary to this branch today 11/04/2023</p><p/> 
+        <p> I just added mary to this branch today 11/04/2023</p>
+        <p />
+        <p>Added dave to this branch</p>
       </header>
     </div>
   );
