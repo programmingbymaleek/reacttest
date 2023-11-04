1b38175be406c0fd8491384ec50be4d9abf6a021
mary <adebimpecole@gmail.com>
2023-11-04 23:06:20 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <p>This is just a test branch 123....</p>
         <p>This is a fantastic project</p>
+        <p> I just added mary to this branch today 11/04/2023</p><p/> 
       </header>
     </div>
   );
